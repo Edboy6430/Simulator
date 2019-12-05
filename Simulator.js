1014e4bf5a9b351fbb19a5a3e3ee3cda66bd5657
Edboy6430 <ek3104@nyu.edu>
2019-12-05 14:11:21 -0500

    Start of Simulator

---
+++ Simulator.js
@@ -0,0 +1,45 @@
+let ball = {};
+let Ambience;
+let Chomp;
+
+var panCount;
+
+function preload()
+{
+  soundFormats('mp3');
+  Chomp = loadSound('Chomp.mp3');
+	Ambience = loadSound('Ambience.mp3');
+	masterVolume(0.2);
+}
+
+function setup()
+{
+  createCanvas(windowWidth, windowHeight);
+	
+	reverb = new p5.Reverb();
+	reverb.process(Chomp, 1.25, 2.5); // p5.Reverb has parameters for time and decay rate
+	// ambienceReverb = new p5.Reverb();
+	// ambienceReverb.process(Ambience, 2, 3);
+	
+	Ambience.play();
+	Ambience.loop();
+}
+
+function draw()
+{
+  background(0);
+	
+  ball.x = constrain(mouseX, 0, width);
+  ellipse(ball.x, (height / 2), 100, 100);
+}
+
+function mousePressed()
+{
+  // map the ball's x location to a panning degree
+  // between -1.0 (left) and 1.0 (right)
+	
+  let panning = map(ball.x, 0, width, -1.0, 1.0);
+	
+  Chomp.pan(panning);
+  Chomp.play();
+}
